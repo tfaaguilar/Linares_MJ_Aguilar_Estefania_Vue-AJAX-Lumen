ae741e10cab2cd4e96024c132c07b7f88b7807e9
tfaaguilar <113370984+tfaaguilar@users.noreply.github.com>
2024-04-01 11:17:38 -0400

    Styling

--- js/main.js
+++ js/main.js
@@ -52,3 +52,19 @@ const disneyland = Vue.createApp({
     }
 });
 disneyland.mount("#app");
+
+
+  //Burger menu
+
+(function(){
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burger-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
+
+	button.addEventListener("click", hamburgerMenu, false);		
+})();
